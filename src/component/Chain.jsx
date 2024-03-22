5cdc4cae1bd149616b42e73bf553258b2bac9dd6
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2024-03-22 21:27:23 +0100

    init

--- src/component/Chain.jsx
+++ src/component/Chain.jsx
@@ -1,9 +1,16 @@
 function Chain() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div>
+      <h1 className="text-center font-bold">On-chain Fix</h1>
+      <p className="">
+        On-chain Fix is an open protocol to communicate securely between Wallets
+        and DApps (Web3 Apps).
+        <br />
+        This protocol establishes a remote connection using a Bridge server.
+      </p>
+      <p>Get Started</p>
+    </div>
+  );
 }
 
-export default Chain
+export default Chain;
