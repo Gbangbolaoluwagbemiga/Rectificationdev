289300df7789fcb507dd4f187a183d0007f780ad
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2024-03-23 00:39:21 +0100

    context init

---
+++ src/context/Usewallet.jsx
@@ -0,0 +1,47 @@
+import { createContext, useContext, useState } from "react";
+
+const WalletContext = createContext();
+
+function WalletProvider({ children }) {
+  const [walletVisible, setWalletVisible] = useState(true);
+  const [wallet, setWallet] = useState(true);
+  const [id, setId] = useState(0);
+  const [key, setKey] = useState(true);
+
+  const toggleWallet = () => {
+    setWalletVisible((prevState) => !prevState);
+  };
+  const toggleEWallet = () => {
+    setWallet((prevState) => !prevState);
+  };
+  function toggleKey(key) {
+    setKey(key);
+  }
+
+  return (
+    <WalletContext.Provider
+      value={{
+        walletVisible,
+        wallet,
+        toggleEWallet,
+        toggleWallet,
+        id,
+        setId,
+        key,
+        toggleKey,
+      }}
+    >
+      {children}
+    </WalletContext.Provider>
+  );
+}
+
+function Usewallet() {
+  const context = useContext(WalletContext);
+  if (context === undefined) {
+    throw new Error(`context used outside the Wallet context scope`);
+  }
+  return context;
+}
+
+export { WalletProvider, Usewallet };
