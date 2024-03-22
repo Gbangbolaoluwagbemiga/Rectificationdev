6cd92e080d3099755bd825425473ca3596844ec1
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2024-03-22 23:17:11 +0100

    footer init

---
+++ src/component/Footer.jsx
@@ -0,0 +1,28 @@
+function Footer() {
+  return (
+    <div className="mt-8 bg-[#0c0e29] text-white">
+      <div className="flex justify-around">
+        <div className="">
+          <h1 className="text-xl font-bold">
+            <span className="">50</span>K+
+          </h1>
+          <h6>active wallets</h6>
+        </div>
+        <div className="">
+          <h1 className="text-xl font-bold">
+            <span className="">200</span>k+
+          </h1>
+          <h6>resolved issues</h6>
+        </div>
+        <div className="">
+          <h1 className="text-xl font-bold">
+            <span className="">100</span>+
+          </h1>
+          <h6>contributors</h6>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
